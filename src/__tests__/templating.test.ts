d47fc493a453685996491558eaab74bcf0b6ac1c
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-10-03 15:08:08 +0200

    Attempt at reproducing issue #154 (IF and FOR commands on the same line)

--- src/__tests__/templating.test.ts
+++ src/__tests__/templating.test.ts
@@ -1097,6 +1097,22 @@ Morbi dignissim consequat ex, non finibus est faucibus sodales. Integer sed just
         );
         expect(result.includes('enigrebua')).toBeTruthy(); // the word aubergine in reverse
       });
+
+      it('IF and FOR on the same line (issue #154)', async () => {
+        const template = await fs.promises.readFile(
+          path.join(__dirname, 'fixtures', 'ifForOnSameLine.docx')
+        );
+
+        const result = await createReport({
+          noSandbox,
+          template,
+          data: {
+            list: ['a', 'b', 'c'],
+          },
+        });
+
+        expect(result).toBeInstanceOf(Uint8Array);
+      });
     });
   });
 });
